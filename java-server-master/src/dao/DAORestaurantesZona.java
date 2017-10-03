2a79b4e4898d08e4e34129238562b474032c2137
macOS Sierra <da.solano1@uniandes.edu.co>
2017-10-03 15:00:33 -0500

    s

--- java-server-master/src/dao/DAORestaurantesZona.java
+++ java-server-master/src/dao/DAORestaurantesZona.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import vos.Ingrediente;
 import vos.Restaurante;
 import vos.Video;
+import vos.Zona;
 
 public class DAORestaurantesZona {
 
@@ -81,7 +82,41 @@ public class DAORestaurantesZona {
 
 	}
 	/**
-	 * 
+	 * GET zonas
+	 * @return
+	 * @throws SQLException
+	 * @throws Exception
 	 */
+	public ArrayList<Zona> darZonas() throws SQLException, Exception {
+		ArrayList<Zona> zonas = new ArrayList<Zona>();
+		String sql = "SELECT * FROM ZONA";
+
+		PreparedStatement prepStmt = conn.prepareStatement(sql);
+		recursos.add(prepStmt);
+		ResultSet rs = prepStmt.executeQuery();
+
+		while (rs.next()) {
+			Long id = rs.getLong("ID"); 
+			Integer capacidadPersonas = rs.getInt("CAPCIDAD_PERSNAS"); 
+			Boolean handicap = rs.getBoolean("HANDICAP"); 
+			String condicionesTecnicas = rs.getString("CONDICIONES_TECNICAS"); 
+			zonas.add(new Zona(id, capacidadPersonas, handicap, condicionesTecnicas)); 
+		}
+		return zonas;
+	}
+	
+	public void addZona(Zona zona) throws SQLException, Exception {
+
+		String sql = "INSERT INTO ZONA VALUES (";
+		sql += zona.getId() + ",'";
+		sql += zona.getCapacidadPersonas() + "',";
+		sql += zona.getHandicap() + "',"; 
+		sql += zona.getCondicionesTecnicas() + ")";
+
+		PreparedStatement prepStmt = conn.prepareStatement(sql);
+		recursos.add(prepStmt);
+		prepStmt.executeQuery();
+
+	}
 	
 }
