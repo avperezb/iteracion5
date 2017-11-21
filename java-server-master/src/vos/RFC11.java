8713b424f393edda5d04db7717f16dfd9c53543e
DIEGO ALEJANDRO SOLANO ARIZA <da.solano1@sis.virtual.uniandes.edu.co>
2017-11-21 15:00:25 -0500

    sss

---
+++ java-server-master/src/vos/RFC11.java
@@ -0,0 +1,102 @@
+package vos;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+public class RFC11 {
+	
+	@JsonProperty(value = "diaDeLaSemana")
+	private String diaDeLaSemana; 
+	
+	@JsonProperty(value = "nombreProductoMasConsumido")
+	private String nombreProductoMasConsumido; 
+	
+	@JsonProperty(value = "cantidadMaximaProducto")
+	private int cantidadMaximaProducto; 
+	
+	@JsonProperty(value = "cantidadMinimaProducto")
+	private int cantidadMinimaProducto; 
+	
+	@JsonProperty(value = "nombreRestMasFrec")
+	private String nombreRestMasFrec; 
+	
+	@JsonProperty(value = "nombreRestMenosFrec")
+	private String nombreRestMenosFrec;
+
+	
+	
+	public RFC11(@JsonProperty(value = "diaDeLaSemana") String diaDeLaSemana, @JsonProperty(value = "nombreProductoMasConsumido") String nombreProductoMasConsumido,@JsonProperty(value = "cantidadMaximaProducto") int cantidadMaximaProducto,
+			@JsonProperty(value = "cantidadMinimaProducto") int cantidadMinimaProducto, @JsonProperty(value = "nombreRestMasFrec") String nombreRestMasFrec,@JsonProperty(value = "nombreRestMenosFrec") String nombreRestMenosFrec) {
+		
+		this.diaDeLaSemana = diaDeLaSemana;
+		this.nombreProductoMasConsumido = nombreProductoMasConsumido;
+		this.cantidadMaximaProducto = cantidadMaximaProducto;
+		this.cantidadMinimaProducto = cantidadMinimaProducto;
+		this.nombreRestMasFrec = nombreRestMasFrec;
+		this.nombreRestMenosFrec = nombreRestMenosFrec;
+	}
+
+
+	public String getDiaDeLaSemana() {
+		return diaDeLaSemana;
+	}
+
+	
+	public void setDiaDeLaSemana(String diaDeLaSemana) {
+		this.diaDeLaSemana = diaDeLaSemana;
+	}
+
+	
+	public String getNombreProductoMasConsumido() {
+		return nombreProductoMasConsumido;
+	}
+	
+
+	public void setNombreProductoMasConsumido(String nombreProductoMasConsumido) {
+		this.nombreProductoMasConsumido = nombreProductoMasConsumido;
+	}
+	
+
+	public int getCantidadMaximaProducto() {
+		return cantidadMaximaProducto;
+	}
+	
+
+	public void setCantidadMaximaProducto(int cantidadMaximaProducto) {
+		this.cantidadMaximaProducto = cantidadMaximaProducto;
+	}
+	
+
+	public int getCantidadMinimaProducto() {
+		return cantidadMinimaProducto;
+	}
+	
+
+	public void setCantidadMinimaProducto(int cantidadMinimaProducto) {
+		this.cantidadMinimaProducto = cantidadMinimaProducto;
+	}
+	
+
+	public String getNombreRestMasFrec() {
+		return nombreRestMasFrec;
+	}
+	
+
+	public void setNombreRestMasFrec(String nombreRestMasFrec) {
+		this.nombreRestMasFrec = nombreRestMasFrec;
+	}
+	
+
+	public String getNombreRestMenosFrec() {
+		return nombreRestMenosFrec;
+	}
+
+	public void setNombreRestMenosFrec(String nombreRestMenosFrec) {
+		this.nombreRestMenosFrec = nombreRestMenosFrec;
+	} 
+	
+	
+	
+	
+	
+
+}
